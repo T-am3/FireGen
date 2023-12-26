f3a1a65ab6d80aae9fd5d45b23c2db6447e5a7dc
Tame <speedyweedypeed@gmail.com>
2023-12-26 13:52:55 -0600

    V1

---
+++ script.js
@@ -0,0 +1,83 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const inputs = document.querySelectorAll('input[type="number"], input[type="checkbox"]');
+    inputs.forEach(input => input.addEventListener('change', generatePassword));
+
+    generatePassword(); // Initial password generation
+});
+
+function generatePassword() {
+    const lengthInput = document.getElementById('passwordLength');
+    const uppercaseCheckbox = document.getElementById('includeUppercase');
+    const numbersCheckbox = document.getElementById('includeNumbers');
+    const symbolsCheckbox = document.getElementById('includeSymbols');
+
+    let passwordLength = parseInt(lengthInput.value, 10); // Parse the value as an integer
+
+    // Ensure password length is between 1 and 20
+    passwordLength = Math.max(1, Math.min(passwordLength, 20));
+
+    lengthInput.value = passwordLength; // Update the input field value
+
+    const includeUppercase = uppercaseCheckbox.checked;
+    const includeNumbers = numbersCheckbox.checked;
+    const includeSymbols = symbolsCheckbox.checked;
+
+    const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+    const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const numberChars = '0123456789';
+    const symbolChars = '!@#$%^&*()_-+=<>?';
+
+    let allChars = lowercaseChars;
+    if (includeUppercase) allChars += uppercaseChars;
+    if (includeNumbers) allChars += numberChars;
+    if (includeSymbols) allChars += symbolChars;
+
+    const password = generateRandomPassword(allChars, passwordLength);
+
+    const passwordResult = document.getElementById('passwordResult');
+    const copiedIndicator = document.getElementById('copiedIndicator');
+
+    passwordResult.innerText = password;
+    copiedIndicator.style.display = 'none';
+
+    copyToClipboard(); // Copy the generated password to clipboard automatically
+
+    const passwordStrength = calculatePasswordStrength(password);
+    updateStrengthBar(passwordStrength);
+}
+
+function generateRandomPassword(characters, length) {
+    let password = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        password += characters.charAt(randomIndex);
+    }
+    return password;
+}
+
+function copyToClipboard() {
+    const passwordResult = document.getElementById('passwordResult');
+    const textarea = document.createElement('textarea');
+    textarea.value = passwordResult.innerText;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textarea);
+
+    const copiedIndicator = document.getElementById('copiedIndicator');
+    copiedIndicator.style.display = 'block';
+    setTimeout(function () {
+        copiedIndicator.style.display = 'none';
+    }, 1500);
+}
+
+function updateStrengthBar(strength) {
+    const strengthBar = document.getElementById('strengthBar');
+    strengthBar.style.width = `${strength}%`;
+}
+
+function calculatePasswordStrength(password) {
+    // Simple password strength calculation based on length
+    const lengthStrength = Math.min(password.length / 20 * 100, 100);
+    return lengthStrength;
+}
